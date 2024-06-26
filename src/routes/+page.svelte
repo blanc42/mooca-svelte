4809a4dc456562c733a3cc4c05f788650654d324
blank1729 <85779539+blank1729@users.noreply.github.com>
2024-06-26 11:58:41 +0530

    first commit - installed tailwind, shadcn

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Button from "$lib/components/ui/button/button.svelte";
+
+</script>
+
+<div class="w-full h-full bg-neutral-950 text-white flex flex-col justify-center items-center">
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <Button on:click={() => console.log("Clicked")}>Click me</Button>
+</div>
+  
